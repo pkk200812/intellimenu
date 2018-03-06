9a0f31ea7ec99b22ef7af29f9521247e01e29eb2
pkk200812 <37025933+pkk200812@users.noreply.github.com>
2018-03-06 17:46:43 -0500

    snd

--- index.js
+++ index.js
@@ -5,19 +5,19 @@ document.getElementById('speaker2').onclick = clickEnglish2();
 document.getElementById('speaker3').onclick = clickEnglish3();
 
 function clickEnglish1() {
-  this.snd = new Audio('english1.mp3');
-  this.snd.play();
-  this.snd.currentTime = 0;
+  var snd = new Audio('english1.mp3');
+  snd.play();
+  snd.currentTime = 0;
 }
 
 function clickEnglish2() {
-  this.snd = new Audio('english2.mp3');
-  this.snd.play();
-  this.snd.currentTime = 0;
+  var snd = new Audio('english2.mp3');
+  snd.play();
+  snd.currentTime = 0;
 }
 
 function clickEnglish3() {
-  this.snd = new Audio('english3.mp3');
-  this.snd.play();
-  this.snd.currentTime = 0;
+  var snd = new Audio('english3.mp3');
+  snd.play();
+  snd.currentTime = 0;
 }
